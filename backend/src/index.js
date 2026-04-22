3124a583000737b1063b90cd0d1a5f9bbc07d3ff
Neha Jadhav <njadhav9044@gmail.com>
2026-04-22 16:05:04 +0530

    added frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ await server.start();
  server.applyMiddleware({ 
     app, 
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:4000"],
+      origin: ["https://extraordinary-blancmange-4e4736.netlify.app/"],
       credentials: true
     }
   });
